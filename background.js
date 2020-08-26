44ec7a6982b828dfc36116fc00304dcd8a21ff9d
Annmarie <43488004+annmarieadams@users.noreply.github.com>
2020-08-26 16:53:57 -0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,18 @@
+function getProfessorPage(info,tab) {
+    if (info.menuItemId !== 'rmpLC') {
+        return;
+    }
+    var professorName = info.selectionText.replace(' ', '+');
+    this.schoolName = this.schoolName.replace(' ', '+')
+    var searchURL = 'http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName='+this.schoolName+'&schoolID='+this.schoolID+'&query=' + professorName;
+    chrome.tabs.create({url: searchURL});
+}
+var schoolID = '1367'; //1367 for Chabot College, 2352 for Las Positas College. 
+var schoolName = 'Chabot College'; //Chabot College or Las Positas College
+chrome.contextMenus.create({
+    title: 'Search for %s on RMP', 
+    contexts:['selection'], 
+    id: 'rmpLC'
+});
+chrome.contextMenus.onClicked.addListener(getProfessorPage);
+
